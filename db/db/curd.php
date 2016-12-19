862a965e0a447671c4af968e0b770d588ee107f7
zhuhaoyi <zhuhaoyi@163.com>
2016-12-19 17:05:21 +0800

    完成删除功能

--- db/db/curd.php
+++ db/db/curd.php
@@ -49,13 +49,19 @@ class DB
         }
     }
 
-    public function delete()
+    public function delete($param)
     {
         $this->type='delete';
         try {
-            $st = $this->pdo->prepare($this->sql);
-            $st->execute();
-            var_dump($st->fetchAll());
+            $st = $this->pdo->exec($this->sql($param));
+            echo $this->sql($param);
+            var_dump($st);
+            if($st==0){
+                echo '删除失败';
+            }elseif($st>0){
+                echo '删除成功';
+            }
+            var_dump($st->errorInfo());
         } catch (\Exception $e) {
             echo $e->getMessage();
         }
@@ -95,7 +101,11 @@ class DB
                     return "insert into $this->table ($keys[0]) values ($values[0])";
                 }
             case($this->type == 'delete'):
-                return "delete from $this->table";
+                //todo 只能使用一个参数 变量类型未加括号
+                    $keys=array_keys($param);
+                    $values=array_values($param);
+                return "delete from $this->table where ($keys[0]) = ($values[0])";
+
             case($this->type == 'update'):
                 return "update $this->table set";
             case($this->type == 'select'):
@@ -142,9 +152,11 @@ class DB
     }
 }
 
-
 exit();
 //todo update delete
 //todo where语句只能输入一个参数 limit不支持0,10写法
 DB::table('ddd')->add(['text'=>111]);
 DB::table('ddd')->where([ 'id' => 1])->limit(1)->get();
+DB::table('ddd')->delete(['text'=>111]);
+
+DB::table('ddd')->where(['text'=>111])->update(['ccc'=>333]);
